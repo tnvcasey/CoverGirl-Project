2c660ecb38b67e27ec67d70cfd51152c1ba5082d
taylorvercillo <taylornvcasey@gmail.com>
2021-12-31 15:51:58 -0700

    making product list within ul.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
     
 })
 const Base_URL = "http://makeup-api.herokuapp.com"
+function getList() {
+    const ul = document.getElementById('product-list')
+    const makeup = document.getElementById("makeup")
+    
+    fetch(Base_URL + '/api/v1/products.json?brand=covergirl')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(product => {
+            ul.innerHTML += `
+               <li>${product.name}</a></li>
+            `
+        })
